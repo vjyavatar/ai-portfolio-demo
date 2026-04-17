b9c7ba7859ade41a0ea0cdb6fda824ec0ad67971
vjyavatar <vjyavatar@users.noreply.github.com>
2026-04-17 21:24:10 +0530

    UI Revamp

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import CelesysApp from "./celesys_app.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <CelesysApp />
+  </React.StrictMode>
+);
